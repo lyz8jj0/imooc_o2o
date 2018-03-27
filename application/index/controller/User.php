b6c7dacb5bada6204fc1c4b1678d0b08f95a2f58
李新宇 <844216765@qq.com>
2018-03-27 15:23:08 +0800

    添加tp5验证码

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -12,6 +12,14 @@ class User extends Controller
 
     public function register()
     {
+        if (request()->isPost()) {
+            $data = input('post.');
+            if (captcha_check($data['verifycode'])) {
+                //校验失败
+                $this->error('验证码不正确');
+            }
+            exit;
+        }
         return $this->fetch();
     }
 }
